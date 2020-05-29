8a8e637bcd36ce300de376672f225ca50dad4aa8
Fabio Frattin <49120896+frattinfabio@users.noreply.github.com>
2020-05-29 12:49:29 +0200

    Update dann_net.py

--- dann_net.py
+++ dann_net.py
@@ -1,6 +1,9 @@
 import torch
 import torch.nn as nn
-from .utils import load_state_dict_from_url
+try:
+    from torch.hub import load_state_dict_from_url
+except ImportError:
+    from torch.utils.model_zoo import load_url as load_state_dict_from_url
 
 
 __all__ = ['AlexNet', 'alexnet']
